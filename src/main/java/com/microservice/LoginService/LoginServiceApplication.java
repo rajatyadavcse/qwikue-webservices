d759c78cebd82b79a3b722c70b137a6a4fe3aa94
rajatyadavcse <rajat.yadav.cse@gmail.com>
2026-05-29 22:24:23 +0530

    feat: configure default application timezone to Asia/Kolkata

--- src/main/java/com/microservice/LoginService/LoginServiceApplication.java
+++ src/main/java/com/microservice/LoginService/LoginServiceApplication.java
@@ -3,10 +3,17 @@ package com.microservice.LoginService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableScheduling
 public class LoginServiceApplication {
+
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LoginServiceApplication.class, args);
